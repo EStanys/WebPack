842dd1de70e5319c322d612f600600d8f46b9000
EStanys <edvinasstanys@gmail.com>
2021-05-20 11:33:21 +0300

    entry output created

---
+++ dist/main.bundle.js
@@ -0,0 +1,5 @@
+(() => {
+  "use strict";
+  var o;
+  console.log("index.js"), console.log("add(1,2,3)", 6), (o = " nice day"), console.log("I am saying" + o);
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,15 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-// is src paima failus(pagal nutylejima index.html ima) ir sudeda i dist
+// is src paima failus(jei nekursime, pagal nutylejima index.html ima) ir sudeda i dist
 module.exports = {
-  entry: {},
-  output: {},
+  // is cia viskas bus paimta
+  // nurodom musu programos pagrindini js faila. is jo webpackas padarys optimalia versija
+  entry: { main: path.resolve(__dirname, "./src/app.js") },
+  output: {
+    // i cia viskas bus sudeta, failo pavadinimas bus sukrtas main.bundle.js
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   plugins: [],
 };
