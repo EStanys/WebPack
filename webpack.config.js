61e5a1ecc5bf9f6900047d3fa9cf827c34761af1
EStanys <edvinasstanys@gmail.com>
2021-05-20 11:09:38 +0300

    webpack.config.js created, html plugin imported

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// cia gules visa webpack konfiguracija
+
+//norim pajungt webpacui html plugina:
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+// is src paima failus(pagal nutylejima index.html ima) ir sudeda i dist
+module.exports = {
+  entry: {},
+  output: {},
+  plugins: [],
+};
