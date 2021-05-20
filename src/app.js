a55451536082116ab9ef57e36f21bc4deaf1ea6d
EStanys <edvinasstanys@gmail.com>
2021-05-20 11:53:25 +0300

    title added dynamically

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ console.log("index.js");
 
 import Add from "./js/Add.js";
 import Say from "./js/Say.js";
+// const path = require("path"); path yra node dalykas
+// console.log(" path", path.resolve(__dirname));
 
 console.log("add(1,2,3)", Add(1, 2, 3));
 Say(" nice day");
+
+const obj = {
+  age: 30,
+  name: "Bob",
+};
+
+const copy = { ...obj, gender: "male" };
+
+console.log(" copy", copy);
