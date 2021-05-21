6969f76a82201836aed5488b1d0f0b8bc1f7c365
EStanys <edvinasstanys@gmail.com>
2021-05-21 10:55:33 +0300

    clean added to wabpack ourput, production wbp js created

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,18 +6,15 @@ const path = require("path");
 
 // is src paima failus(jei nekursime, pagal nutylejima index.html ima) ir sudeda i dist
 module.exports = {
-  mode: "development",
-  devtool: "source-map", // devtool:false - kad kai sukompiliuos dev geriau eitu suprast kas parasyta
-  //devtool: "source-map" - kad consolog'e rodytu is kuriu failu kas ateina, nes kitaip rodytu kad viskas yra ivykdoma main.bundle faile
-  devServer: {
-    contentBase: "./dist",
-  },
+  mode: "production",
+  // devtool: "source-map",
+
   // is cia viskas bus paimta
   // nurodom musu programos pagrindini js faila. is jo webpackas padarys optimalia versija
   entry: { main: path.resolve(__dirname, "./src/app.js") },
   output: {
     // i cia viskas bus sudeta, failo pavadinimas bus sukrtas main.bundle.js
-    filename: "main.bundle.js",
+    filename: "final.bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true, // istrins dist failus, pries irasant naujus
   },
